04fe38c99458b355e095932b165617d2cecf4ed0
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-27 09:22:08 -0600

    Initial commit, complete patron and exhibit class

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,11 @@
+require "./lib/exhibit"
+require "./lib/patron"
+
+RSpec.describe Exhibit do
+  let(:exhibit) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
+
+  it 'exists' do
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
+end
---
+++ spec/patron_spec.rb
@@ -0,0 +1,21 @@
+require "./lib/exhibit"
+require "./lib/patron"
+
+RSpec.describe Patron do
+  let(:patron_1) { Patron.new("Bob", 20) }
+
+  it 'exists' do
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+  end
+
+  it 'can have interests' do
+
+    expect(patron_1.interests).to eq([])
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+end
