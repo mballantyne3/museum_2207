04e8dc758d23b9626bab83518c36ec6c60d4e40a
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-27 10:01:15 -0600

    complete iteration 2

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Patron do
   let(:patron_1) { Patron.new("Bob", 20) }
 
   it 'exists' do
+    expect(patron_1).to be_instance_of(Patron)
     expect(patron_1.name).to eq("Bob")
     expect(patron_1.spending_money).to eq(20)
   end
